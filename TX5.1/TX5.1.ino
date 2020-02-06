6d01c8886e0e2fa5db0be73f048f58a4560313db
kntzn <kntznart@gmail.com>
2020-02-06 20:51:54 +0300

    Add project files.

---
+++ TX5.1/TX5.1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TX5.ino
+ Created:	2/6/2020 8:51:50 PM
+ Author:	kntzn
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
